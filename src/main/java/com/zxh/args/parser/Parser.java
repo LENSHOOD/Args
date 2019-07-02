55e702663e1428eee376e21d204811973f817bf0
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-07-02 13:28:01 +0800

    feat: ugly parser with base test

--- src/main/java/com/zxh/args/parser/Parser.java
+++ src/main/java/com/zxh/args/parser/Parser.java
@@ -1,4 +1,13 @@
-package com.zxh.args.parser.internal;
+package com.zxh.args.parser;
+
+import com.zxh.args.parser.internal.Analyzer;
+import com.zxh.args.parser.internal.Schema;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Parser:
@@ -6,4 +15,56 @@ package com.zxh.args.parser.internal;
  * @date 2019-07-01
 */
 public class Parser {
+
+    private Schema schema;
+    private Analyzer analyzer = new Analyzer();
+
+    public Parser(String schemaFileAddr) {
+        schema = new Schema(schemaFileAddr);
+    }
+
+    public ParserResult parse(String args) {
+        ParserResult result = new ParserResult();
+
+        List<String[]> flagAndStringValuePairs = analyzer.analyze(args);
+        flagAndStringValuePairs.forEach(
+                flagAndStringValue -> {
+                    String flag = flagAndStringValue[0];
+                    String valueString = flagAndStringValue[1];
+                    Class clazz = schema.getFlagClass(flag);
+
+                    try {
+                        Object value;
+                        if (clazz == Boolean.class && valueString.isEmpty()) {
+                            valueString = "true";
+                        }
+                        if (clazz == String.class) {
+                            value = valueString;
+                        } else if (clazz.isArray()) {
+                            String[] valueStringArray = valueString.split(",");
+                            Object valueArray = Array.newInstance(clazz.getComponentType(), valueStringArray.length);
+                            for (int i = 0; i < valueStringArray.length; i++) {
+                                Class componentClass = clazz.getComponentType();
+                                if (componentClass == String.class) {
+                                    Array.set(valueArray, i, valueStringArray[i]);
+                                } else {
+                                    Array.set(valueArray, i, componentClass.getMethod("valueOf", String.class).invoke(null, valueStringArray[i]));
+                                }
+                            }
+                            value = valueArray;
+                        }
+                        else {
+                            value = clazz.getMethod("valueOf", String.class).invoke(null, valueString);
+                        }
+
+                        result.setFlagValue(flag, value);
+                    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException();
+                    }
+                }
+        );
+
+        return result;
+    }
 }
--- src/test/java/com/zxh/args/parser/ParserTest.java
+++ src/test/java/com/zxh/args/parser/ParserTest.java
@@ -1,5 +1,29 @@
+package com.zxh.args.parser;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ParserTest {
 
+    private Parser parser;
+
+    @Test
+    public void validate_parse() {
+        // normal input
+        String args = "-l -p 8080 -d /usr/logs";
+        parser = new Parser(getClass().getClassLoader().getResource("test-schema-1.yml").getPath());
+        ParserResult normalResult = parser.parse(args);
+        assertEquals(true, normalResult.getValueByFlag("l"));
+        assertEquals(8080, normalResult.getValueByFlag("p"));
+        assertEquals("/usr/logs", normalResult.getValueByFlag("d"));
+
+        // array input
+        String arrayArgs = "-i 1,2,3 -d 1.0,2.1,3.2 -s 1,2.0,3ab";
+        parser = new Parser(getClass().getClassLoader().getResource("test-schema-array.yml").getPath());
+        ParserResult arrayResult = parser.parse(arrayArgs);
+        assertArrayEquals(new Integer[]{1,2,3}, (Integer[])arrayResult.getValueByFlag("i"));
+        assertArrayEquals(new Double[]{1.0,2.1,3.2}, (Double[])arrayResult.getValueByFlag("d"));
+        assertArrayEquals(new String[]{"1","2.0","3ab"}, (String[])arrayResult.getValueByFlag("s"));
+    }
 }
