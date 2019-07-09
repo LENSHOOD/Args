c2484f0c6b196a2218d8ae6ca94f824f7f27c126
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-07-10 00:01:34 +0800

    feat: given schem

---
+++ src/main/java/zxh/demo/args/internal/exception/BuildSchemaException.java
@@ -0,0 +1,12 @@
+package zxh.demo.args.internal.exception;
+
+/**
+ * BuildSchemaException:
+ * @author zhangxuhai
+ * @date 2019-07-09
+*/
+public class BuildSchemaException extends RuntimeException {
+    public BuildSchemaException(String message) {
+        super(message);
+    }
+}
